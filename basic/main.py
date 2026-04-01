19b541b2ab5a9ac3eddd01c523ddc88b961b656e
pgmrDohan <dohan.k764@gmail.com>
2026-04-01 18:07:25 +0900

    260326 권도한 과제 제출

---
+++ basic/main.py
@@ -0,0 +1,30 @@
+def get_names():
+    name_list = []
+
+    while True:
+        name = input("✏️  아기 사자 이름을 입력하세요 (종료하려면 q 입력): ")
+
+        if name == "q":
+            print()
+            print("📌 이름 입력을 종료합니다.")
+            break
+
+        if name == "":
+            print("⚠️  이름이 비어있습니다. 다시 입력해주세요.")
+            continue
+
+        name_list.append(name)
+        print(f"✅ '{name}' 이(가) 등록되었습니다.")
+
+    return name_list
+
+def print_names(name_list):
+    print()
+    print("📋 현재 아기 사자 명단입니다.")
+
+    for i in range(len(name_list)):
+        print(f"🦁 {i + 1}. {name_list[i]}")
+
+print("🦁 아기 사자 명단 관리 프로그램입니다.")
+name_list = get_names()
+print_names(name_list)
