52961585a4cd7c3a4a168f04361397d01a58d230
Wenric-min <opk324642@gmail.com>
2026-04-02 13:04:19 +0900

    Wenric-min 프로필 과제 제출

---
+++ profile/week1.py
@@ -0,0 +1,19 @@
+print("아기 사자 명단 관리 프로그램입니다.")
+
+lion_list = []
+
+while 1:
+    name = input(" 아기 사자 이름을 입력하세요 (종료하려면 q 입력): ").strip()
+    
+    if name == 'q':
+        print("\n이름 입력을 종료합니다.\n")
+        break
+    elif not name:
+        print("이름이 비어있습니다. 다시 입력해주세요.")
+    else:
+        lion_list.append(name)
+        print(f"'{name}' 이(가) 등록되었습니다.")
+        
+print("현재 아기 사자 명단입니다.")
+for i, lion in enumerate(lion_list, 1):
+    print(f"{i}. {lion}")
